583ed29c542f81ed3f64937211796ef003d047e3
Burhan Arif M <burhan.arifm@hotmail.com>
2019-06-22 22:44:54 +0700

    add JenisSurat model

---
+++ database/migrations/2019_06_22_223624_create_jenis_surats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJenisSuratsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jenis_surat', function (Blueprint $table) {
+            $table->string('kode_surat');
+            $table->string('jenis_surat');
+            $table->timestamps();
+
+            $table->primary('kode_surat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jenis_surat');
+    }
+}
